143c1449f85745876ce03d3ae6b1a9fbf262cc80
Aleksandr Shreyner <al.shreyner@gmail.com>
2021-08-23 20:30:38 +0300

    Add Gracefully and add fastify

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,20 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
+const delay = (ms = 1000) => new Promise((resolve) => setTimeout(resolve, ms));
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
-  getHello(): string {
+  getHello() {
     return this.appService.getHello();
+  }
+
+  @Get('/delay')
+  async delay() {
+    await delay(10 * 1000);
+
+    return { status: true };
   }
 }
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,30 @@
 import { NestFactory } from '@nestjs/core';
+import {
+  FastifyAdapter,
+  NestFastifyApplication,
+} from '@nestjs/platform-fastify';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const fastify = new FastifyAdapter({ logger: true });
+
+  const app = await NestFactory.create<NestFastifyApplication>(
+    AppModule,
+    fastify,
+  );
+
+  await app.listen(3000, '0.0.0.0');
+
+  async function closeGracefully(signal: unknown) {
+    console.log(`Received signal to terminate: ${signal} ...`);
+    await app.close();
+    console.log(`Stop!`);
+
+    process.exit(0);
+  }
+
+  process.on('SIGINT', closeGracefully);
+  process.on('SIGTERM', closeGracefully);
 }
+
 bootstrap();
